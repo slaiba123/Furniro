1c6de164b2697017a7f017ef7a3d3b9fcd9dec39
slaiba123 <139244200+slaiba123@users.noreply.github.com>
2025-01-27 23:11:55 +0500

    !!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/footer";
-import { Poppins, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 // Import Poppins with specific weights
 const poppins = Poppins({
